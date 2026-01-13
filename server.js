75a4fc554748984df9288a30729999217d7638ce
dlxs1-2 <mail.dlxs1@gmail.com>
2026-01-13 23:35:57 +0100

    Update server.js

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ const server = http.createServer((req, res) => {
   res.end('Hello world\n');
 });
 
+setInterval(console.log(Devlouix!),3000);
+
 server.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`);
 });
