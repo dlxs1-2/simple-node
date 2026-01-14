0ff619e00268c3621669bed8ae33be8a9191e618
dlxs1-2 <mail.dlxs1@gmail.com>
2026-01-14 20:23:10 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import http from 'http';
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;  
 
 const server = http.createServer((req, res) => {
   if (req.url === '/health') {
