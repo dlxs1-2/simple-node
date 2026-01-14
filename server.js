043773170011c5c50b156fd016002ab08b1afb27
dlxs1-2 <mail.dlxs1@gmail.com>
2026-01-14 20:44:14 +0100

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify({ status: 'ok' }));
     return;
-  }
+  } 
 
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello world\n');
